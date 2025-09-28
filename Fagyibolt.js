e776c09312533575f642e7961a14c1767e8e3b0e
11.D._3-as_csoport <sixtyseven67@freemail.hu>
2025-09-29 00:19:12 +0200

    Add files for webpage

---
+++ Fagyibolt.js
@@ -0,0 +1,10 @@
+function fullname(){
+    let nev = document.getElementById("fullname").value;
+    alert("Szia "+nev+"!");
+}
+
+function telnumber(){
+    let phonenumber = document.getElementById("telnumber").value;
+    alert("A telefonszámod "+phonenumber+"!");
+}
+
