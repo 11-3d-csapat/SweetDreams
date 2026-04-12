1acb7f1c0cda068f2af6ccf47ffcc65b66e19018
11.D._3-as_csoport <sixtyseven67@freemail.hu>
2026-04-12 23:02:38 +0200

    Update server.js

--- server.js
+++ server.js
@@ -20,34 +20,3 @@ app.get('/fagylaltok', (req, res) => {
 });
 
 app.listen(PORT, () => console.log(`http://localhost:${PORT}`));
-
-app.get('/rendelesek', (req, res) => {
-    db.query('SELECT * FROM rendelesek ORDER BY datum DESC', (err, results) => {
-        if (err) {
-            res.status(500).json({ error: err.message });
-        } else {
-            res.json(results);
-        }
-    });
-});
-
-app.get('/api/fagylaltok', (req, res) => {
-    db.query('SELECT * FROM fagylaltok', (err, results) => {
-        err ? res.status(500).json({ error: err.message }) : res.json(results);
-    });
-});
-
-app.post('/api/fagylaltok', (req, res) => {
-    const { nev, ar_kis, ar_nagy } = req.body;
-    db.query('INSERT INTO fagylaltok (nev, ar_kis, ar_nagy) VALUES (?, ?, ?)',
-        [nev, ar_kis, ar_nagy],
-        (err, result) => {
-            err ? res.status(500).json({ error: err.message }) : res.json({ id: result.insertId });
-        });
-});
-
-app.delete('/api/fagylaltok/:id', (req, res) => {
-    db.query('DELETE FROM fagylaltok WHERE id = ?', [req.params.id], (err) => {
-        err ? res.status(500).json({ error: err.message }) : res.json({ success: true });
-    });
-});
